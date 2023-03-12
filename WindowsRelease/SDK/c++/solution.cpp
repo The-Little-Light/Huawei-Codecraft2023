0fa252559c9cc7d7715cefcc6334acd1bbf16264
xiezx23 <xiezx23@mail2.sysu.edu.cn>
2023-03-13 00:05:06 +0800

    3/12 bassline

--- WindowsRelease/SDK/c++/solution.cpp
+++ WindowsRelease/SDK/c++/solution.cpp
@@ -1,6 +1,85 @@
 #include "solution.hpp"
-using namespace std;
+
+/******************************
+author:     xiezx
+date:       2023-3-11
+describe:   第一版bassline
+******************************/
+
+double para1 = 1;
+double para2 = 100;
+double para3 = 1;
+
+bool cmp(misson& a, misson& b) {
+    return a.v > b.v;
+}
+
+void findMission(vector<misson>& msNode) {
+    for (int wbIdx = 0; wbIdx < K; ++wbIdx) {
+        // 寻找有现成产品的工作台
+        if (wb[wbIdx].pstatus) {
+            int proType = wb[wbIdx].type;
+            // 遍历收购方
+            for (auto buyWbIdx: type2BuyIndex[proType]) {
+                // 对应原材料格为空
+                if (!wb[buyWbIdx].checkHaveProType(proType)) {
+                    // 此时从 wbIdx 到 buyWbIdx 是一个潜在任务
+                    msNode.push_back(misson(wbIdx, buyWbIdx, proType));
+                }
+            }
+        }
+    }
+    sort(msNode.begin(), msNode.end(), cmp);
+}
+
+void robot::checkDest() {
+    if (!taskQueue.empty()) {
+        task& curTask = taskQueue.front();
+        if (wb_id == curTask.destId) {
+            // 到达当前工作目的地，交付工作
+            cmd.buy = curTask.buy;
+            cmd.sell = curTask.sell;
+            taskQueue.pop();
+        }
+    }
+}
+
+void robot::checkTask() {
+    if (taskQueue.empty()) {
+        // 分配新任务
+        // 无空闲任务时如何处理？
+        vector<misson> msNode; // 任务节点
+        findMission(msNode);
+        if (msNode.size() == 0) return;
+        misson selected = msNode[0];
+        curMisson = selected;
+        taskQueue.push(task(wb[selected.startIndex].location ,selected.startIndex));
+        taskQueue.push(task(wb[selected.endIndex].location ,selected.endIndex));
+        wb[curMisson.startIndex].pstatus = 0;
+        wb[curMisson.startIndex].setProType(curMisson.proType);
+    }
+    // 执行当前任务，前往目的地
+    task& curTask = taskQueue.front();
+    setSpeed(curTask.destCo);
+}
 
 void solution() {
+    // 根据已分配任务把工作台信息进行同步
+    for (int rtIdx = 0; rtIdx < 4; ++rtIdx) {
+        if (rt[rtIdx].taskQueue.size() == 2) {
+            misson& tmp = rt[rtIdx].curMisson;
+            wb[tmp.startIndex].pstatus = 0;
+            wb[tmp.startIndex].setProType(tmp.proType);
+        }
+        else if (rt[rtIdx].taskQueue.size() == 1) {
+            misson& tmp = rt[rtIdx].curMisson;
+            wb[tmp.startIndex].setProType(tmp.proType);
+        }
+    }
+    // 指令规划
+    for (int rtIdx = 0; rtIdx < 4; ++rtIdx) {
+        rt[rtIdx].checkDest();
+        rt[rtIdx].checkTask();
+    }
     return;
 }
--- WindowsRelease/SDK/c++/solution.hpp
+++ WindowsRelease/SDK/c++/solution.hpp
@@ -16,14 +16,64 @@ struct workbench {
     int rtime;   // 剩余生产时间 Remaining production time
     int rstatus; // 原材料格状态 Raw—material status
     int pstatus; // 产品格状态   Product status
+
+    // 判断原材料proType型号格是否为空，空返回false
+    bool checkHaveProType(int proType) {
+        int s = 1;
+        while(proType--) s <<= 1;
+        return s & rstatus;
+    }
+    void setProType(int proType) {
+        int s = 1;
+        while(proType--) s <<= 1;
+        rstatus = s | rstatus;
+    }
 };
 
-struct command {
+struct command { // 汇总当前帧机器人的控制指令
     double forward;
     double rotate;
     bool buy = false;
     bool sell = false;
     bool destroy = false;
+};
+
+struct task { // 机器人的当前目标工作
+    coordinate destCo;  // 目的坐标
+    int destId;         // 目标工作台下标
+    bool buy = false;
+    bool sell = false;
+    task(coordinate c, int d) {
+        destCo = c;
+        destId = d;
+    }
+};
+
+
+/* 
+定义任务 m(A, x, B) 表示把物品x从A工作台购入并出售给B工作台
+任务价值函数 v(m) 表示完成任务m可以获得的潜在收益
+    1：v 跟x在AB的差价正相关
+    2：跟B的剩余原材料格、AB路径负相关
+*/
+struct misson {
+    int startIndex; // 起点工作台下标
+    int endIndex;   // 终点工作台下标
+    int proType;    // 产品型号
+    double v = 0;   // 价值函数
+
+    double dis(int s, int e) {
+        coordinate& c1 = wb[s].location;
+        coordinate& c2 = wb[e].location;
+        double ans = (c1.x - c2.x)*(c1.x - c2.x) + (c1.y - c2.y)*(c1.y - c2.y);
+        return sqrt(ans);
+    }
+    misson(int s, int e, int p) {
+        startIndex = s;
+        endIndex = e;
+        proType = p;
+        v = profitAndTime[p].first * para1 + para2 / dis(s,e);
+    }
 };
 
 struct robot {
@@ -36,11 +86,21 @@ struct robot {
     double toward; // 朝向，弧度制
     coordinate location;
     command cmd;  // 当前帧要发布的控制指令
+    void setSpeed(coordinate dest); // 负责从当前位置移动到目的地的线速度和角速度指令
+
+    queue<task> taskQueue; // 任务队列
+    misson curMisson;
+    void checkDest();
+    void checkTask();
 };
 
 int K;                         // 工作台数
 robot rt[ROBOT_SIZE];          // 机器人
 workbench wb[WORKBENCH_SIZE];  // 工作台
 char plat[MAP_SIZE][MAP_SIZE]; // 输入地图
+
+map<int, vector<int>> type2BuyIndex; // 根据产品类型寻找收购方下标
+
+pair<int,int> profitAndTime[8];
 
 void solution();
